98d64f52b224d88ea8a919ef8d9685eccd64e2bb
Punleu CHOMNAN <punleuchomnan@gmail.com>
2016-11-21 22:53:19 +0700

    init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+import { NavController , Platform} from 'ionic-angular';
+import { Camera, CardIO } from 'ionic-native';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public platform: Platform) {
+  		this.platform.ready()
+  		.then(() =>{
+
+  			
+
+  		});
+
+  }
+
+
+  showCamera(){
+
+    alert('start Cardio');
+
+
+    	/*Camera.getPicture().then((imageData) => {
+			 // imageData is either a base64 encoded string or a file URI
+			 // If it's base64:
+			 let base64Image = 'data:image/jpeg;base64,' + imageData;
+			}, (err) => {
+			 	
+			});
+
+    	*/
+   
+      CardIO.canScan()
+      .then(
+        (res: boolean) => {
+     //     if(res){
+       //     let options = {
+         //     requireExpiry: true,
+           //   requireCCV: false,
+             // requirePostalCode: false
+            //};
+            //CardIO.scan(options);
+          //}          
+          alert('Can scan Cardio');
+        })
+      .catch(function(error) {
+        alert(JSON.stringify(error));
+      });
+
+
+     
+
+    alert('run Cardio completed');
+  }
+
+}
